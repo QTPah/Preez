1a8cb40dfb89b6914080ed0510db951bc50b9f2a
Your Name <you@example.com>
2024-09-22 17:43:59 +0200

    fix: Reduce access token expiration time to 1 minute

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 const router = express.Router();
 
 const generateTokens = (userId) => {
-  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '15m' });
+  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1m' });
   const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
   return { accessToken, refreshToken };
 };

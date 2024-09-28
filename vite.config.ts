a2383c4241e8f3264c123ad447662c2f9d06fa2c
Your Name <you@example.com>
2024-09-28 12:07:47 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Your API server
+        changeOrigin: true,              // Changes the origin of the host header to the target URL
+      }
+    }
+  }
 });
